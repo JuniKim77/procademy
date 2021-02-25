4b9d462a36a3ed018429ae268cb589073d71ff4a
김호준 <hojun3775@gmail.com>
2021-02-25 14:29:52 +0900

    # MyProfiler 프레임 추가

--- MyProfiler/MyProfiler/MyProfiler.cpp
+++ MyProfiler/MyProfiler/MyProfiler.cpp
@@ -1,1 +1,17 @@
 #include "MyProfiler.h"
+
+void ProfileBegin(WCHAR* szName)
+{
+}
+
+void ProfileEnd(WCHAR* szName)
+{
+}
+
+void ProfileDataOutText(WCHAR* szFileName)
+{
+}
+
+void ProfileReset(void)
+{
+}
--- MyProfiler/MyProfiler/main.cpp
+++ MyProfiler/MyProfiler/main.cpp
@@ -1,5 +1,17 @@
+#include <Windows.h>
+#include <stdio.h>
+
 int main()
 {
+	LARGE_INTEGER Start;
+	LARGE_INTEGER End;
+	LARGE_INTEGER Freq;
+	QueryPerformanceFrequency(&Freq);	// 1초의 진동주기
 
+	QueryPerformanceCounter(&Start);
+	Sleep(1000);
+	QueryPerformanceCounter(&End);
+
+	printf("%lld", End.QuadPart - Start.QuadPart);
 	return 0;
 }
