22ca041a733cb7de877d7470f6b70ba658422c95
김호준 <hojun3775@gmail.com>
2021-03-23 22:06:18 +0900

    # 더블 버퍼링 추가

--- WindowProgrammingPractice/WindowProgrammingPractice/WindowProgrammingPractice.cpp
+++ WindowProgrammingPractice/WindowProgrammingPractice/WindowProgrammingPractice.cpp
@@ -8,6 +8,9 @@
 #include <time.h>
 
 // 전역 변수:
+HDC g_hMemDC;
+HBITMAP g_hMemBitmap;
+HBITMAP g_hMemBitmapOld;
 HPEN gPen = CreatePen(PS_SOLID, 1, RGB(255, 0, 0));
 
 #define QUEUE_SIZE (100)
@@ -70,6 +73,10 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 		DispatchMessage(&msg);
 	}
 
+	SelectObject(g_hMemDC, g_hMemBitmapOld); // DC와 해당 비트맵을 연결
+	DeleteObject(g_hMemBitmap); // 비트맵 삭제
+	DeleteObject(g_hMemDC); // DC 삭제
+
 	return (int)msg.wParam;
 }
 
@@ -87,11 +94,30 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
+	case WM_LBUTTONDOWN:
+	{
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		HDC hdc = GetDC(hWnd);
+
+		TextOut(hdc, rect.right / 2, rect.bottom / 2, L"안녕하세요", 5);
+		break;
+	}
 	case WM_CREATE:
 	{
 		HDC hdc = GetDC(hWnd);
+		RECT Rect;
+
 		SetTimer(hWnd, 1, 50, nullptr);
-		SelectObject(hdc, gPen);
+
+		GetClientRect(hWnd, &Rect); // 현 윈도우와 같은 사이즈를 얻어옴
+		g_hMemDC = CreateCompatibleDC(hdc); // 현재 윈도우에서 사용하고 있는 색상과 같은 걸로 만들어짐 비트맵에 접근하위한 DC임
+		g_hMemBitmap = CreateCompatibleBitmap(hdc, Rect.right, Rect.bottom); // 받아온 크기와 같은 크기의 bitMap 생성
+		g_hMemBitmapOld = (HBITMAP)SelectObject(g_hMemDC, g_hMemBitmap); // 아까 생성성 DC와 비트맵을 연결합니다.
+		ReleaseDC(hWnd, hdc);
+
+		PatBlt(g_hMemDC, 0, 0, Rect.right, Rect.bottom, WHITENESS); // 해당 비트맵을 하얀색 브러쉬로 칠합니다.
+
 		break;
 	}
 	case WM_TIMER:
@@ -100,10 +126,13 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		{
 		case 1:
 		{
-			int y = 250 + rand() % 200;
+			RECT rect;
+			GetClientRect(hWnd, &rect); // 윈도우 창의 크기를 얻음
+
+			int y = rect.bottom / 2 + (rand() % rect.bottom) / 2 - rect.bottom / 4; // 높이를 랜덤으로 만들고
 
 			enqueue(y);
-			InvalidateRect(hWnd, nullptr, true);
+			InvalidateRect(hWnd, nullptr, false); // WM_PAINT를 호출하되 잔상을 지우지 않음 -> 깜박임이 없어짐
 			break;
 		}
 		default:
@@ -127,19 +156,27 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	case WM_PAINT:
 	{
 		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+		RECT rect;
+		GetClientRect(hWnd, &rect);
+		PatBlt(g_hMemDC, 0, 0, rect.right, rect.bottom, WHITENESS); // 비트맵을 하얗게 브러쉬로 칠함
+
 		int y = peakqueue(queueFront);
 		int count = 0;
-		SelectObject(hdc, gPen);
-		MoveToEx(hdc, count, y, nullptr);
-
+		SelectObject(g_hMemDC, gPen); // 펜 지정
+		MoveToEx(g_hMemDC, count, y, nullptr); // 비트맵 DC 에 그리기 시작
 		for (int i = queueFront; i != queueRear; i = (i + 1) % QUEUE_SIZE)
 		{
 			count += 10;
 			y = peakqueue(i);
-			LineTo(hdc, count, y);
+			LineTo(g_hMemDC, count, y);
 		}
+
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
+
+		BitBlt(hdc, 0, 0, rect.right, rect.bottom, g_hMemDC, 0, 0, SRCCOPY); // 완성된 비트맵을 그대로 복사합니다. 이는 SRCCOPY 라는 옵션임
+		// 다른 옵션을 선택하면 다른 결과가 나오니 주의
+		// SRCCOPY : Copies the source rectangle directly to the destination rectangle.
 
 		EndPaint(hWnd, &ps);
 	}
