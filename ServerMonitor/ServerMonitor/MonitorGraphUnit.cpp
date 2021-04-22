6ecf2c51dd112dc8d1d927518855ed8bb30adc10
김호준 <hojun3775@gmail.com>
2021-04-22 21:53:58 +0900

    # 서버 모니터링 클래스 제작 시작

--- ServerMonitor/ServerMonitor/MonitorGraphUnit.cpp
+++ ServerMonitor/ServerMonitor/MonitorGraphUnit.cpp
@@ -1,1 +1,29 @@
 #include "MonitorGraphUnit.h"
+
+MonitorGraphUnit::MonitorGraphUnit(HINSTANCE hInstance, HWND hWndParent, TYPE enType, int iPosX, int iPosY, int iWidth, int iHeight)
+{
+}
+
+MonitorGraphUnit::~MonitorGraphUnit()
+{
+}
+
+LRESULT MonitorGraphUnit::WndProc(HWND hWnd, UINT meesage, WPARAM wParam, LPARAM lParam)
+{
+	return LRESULT();
+}
+
+BOOL MonitorGraphUnit::InsertData(int iData)
+{
+	return 0;
+}
+
+BOOL MonitorGraphUnit::PutThis(void)
+{
+	return 0;
+}
+
+MonitorGraphUnit* MonitorGraphUnit::GetThis(HWND hWnd)
+{
+	return nullptr;
+}
