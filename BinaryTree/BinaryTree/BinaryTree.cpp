ba55b1d30f5dc6ec0f50287d3d70d68ece082bce
김호준 <hojun3775@gmail.com>
2021-06-01 12:40:01 +0900

    # BTree 노드 삭제 기능 완료

--- BinaryTree/BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree/BinaryTree.cpp
@@ -30,7 +30,10 @@ void BinaryTree::InsertNode(int data)
 		{
 			if (node->left == nullptr)
 			{
-				node->left = new Node(data);
+				Node* newNode = new Node(data);
+				node->left = newNode;
+				newNode->parent = node;
+
 				return;
 			}
 
@@ -40,7 +43,10 @@ void BinaryTree::InsertNode(int data)
 		{
 			if (node->right == nullptr)
 			{
-				node->right = new Node(data);
+				Node* newNode = new Node(data);
+				node->right = newNode;
+				newNode->parent = node;
+
 				return;
 			}
 
@@ -52,6 +58,173 @@ void BinaryTree::InsertNode(int data)
 void BinaryTree::printTree()
 {
 	printHelper(mRoot, 0);
+}
+
+bool BinaryTree::DeleteNode(int data)
+{
+	Node* pNode = SearchHelper(data);
+
+	if (pNode == nullptr)
+		return false;
+
+	// 데이터가 발견된 경우
+	// 양 노드를 다 갖고 있는 경우
+	if (pNode->left != nullptr && pNode->right != nullptr)
+	{
+		Node* rightMin = SearchMin(pNode->right);
+
+		pNode->data = rightMin->data;
+		Node* parent = rightMin->parent;
+
+		// 빈 노드 삭제
+		if (parent->left == rightMin)
+		{
+			parent->left = rightMin->right;
+		}
+		else
+		{
+			parent->right = rightMin->right;
+		}
+
+		if (rightMin->right != nullptr)
+		{
+			rightMin->right->parent = parent;
+		}
+
+		delete rightMin;
+	}
+	// 하나의 자식만 갖은 경우
+	else if (pNode->left != nullptr)
+	{
+		if (pNode == mRoot)
+		{
+			mRoot = pNode->left;
+			delete pNode;
+
+			return true;
+		}
+
+		Node* parent = pNode->parent;
+
+		if (parent->right == pNode)
+		{
+			parent->right = pNode->left;
+		}
+		else
+		{
+			parent->left = pNode->left;
+		}
+
+		pNode->left->parent = parent;
+
+		delete pNode;
+	}
+	else if (pNode->right != nullptr)
+	{
+		if (pNode == mRoot)
+		{
+			mRoot = pNode->right;
+			delete pNode;
+
+			return true;
+		}
+
+		Node* parent = pNode->parent;
+
+		if (parent->right == pNode)
+		{
+			parent->right = pNode->right;
+		}
+		else
+		{
+			parent->left = pNode->right;
+		}
+
+		pNode->right->parent = parent;
+
+		delete pNode;
+	}
+	// 아무 자식 없는 경우
+	else
+	{
+		if (pNode == mRoot)
+		{
+			mRoot = nullptr;
+			delete pNode;
+
+			return true;
+		}
+
+		if (pNode->parent->right == pNode)
+		{
+			pNode->parent->right = nullptr;
+		}
+		else
+		{
+			pNode->parent->left = nullptr;
+		}
+
+		delete pNode;
+	}
+
+	return true;
+}
+
+BinaryTree::Node* BinaryTree::SearchHelper(int data)
+{
+	Node* pNode = mRoot;
+
+	while (pNode != nullptr)
+	{
+		if (pNode->data == data)
+		{
+			return pNode;
+		}
+		else if (pNode->data > data)
+		{
+			pNode = pNode->left;
+		}
+		else
+		{
+			pNode = pNode->right;
+		}
+	}
+
+	return nullptr;
+}
+
+BinaryTree::Node* BinaryTree::SearchMax(Node* root)
+{
+	Node* pNode = root;
+
+	while (pNode != nullptr)
+	{
+		if (pNode->right == nullptr)
+		{
+			return pNode;
+		}
+
+		pNode = pNode->right;
+	}
+
+	return nullptr;
+}
+
+BinaryTree::Node* BinaryTree::SearchMin(Node* root)
+{
+	Node* pNode = root;
+
+	while (pNode != nullptr)
+	{
+		if (pNode->left == nullptr)
+		{
+			return pNode;
+		}
+
+		pNode = pNode->left;
+	}
+
+	return nullptr;
 }
 
 void BinaryTree::printHelper(Node* root, int depth)
--- BinaryTree/BinaryTree/BinaryTree.h
+++ BinaryTree/BinaryTree/BinaryTree.h
@@ -7,11 +7,13 @@ private:
 	{
 		int data;
 
+		Node* parent;
 		Node* left;
 		Node* right;
 
 		Node(int data)
 			: data(data)
+			, parent(nullptr)
 			, left(nullptr)
 			, right(nullptr)
 		{
@@ -23,8 +25,13 @@ public:
 	~BinaryTree();
 	void InsertNode(int data);
 	void printTree();
+	bool DeleteNode(int data);
+	
 
 private:
+	Node* SearchHelper(int data);
+	Node* SearchMax(Node* root);
+	Node* SearchMin(Node* root);
 	void printHelper(Node* root, int depth);
 	void deleteHelper(Node* root);
 
