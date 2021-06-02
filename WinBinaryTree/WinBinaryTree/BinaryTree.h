62e69735de384c0583bcfb1b6e1e07cdbf59eb49
hojun3775 <hojun3775@gmail.com>
2021-06-02 17:09:11 +0900

    # 바이너리 서치 트리 뷰어 작업 시작

---
+++ WinBinaryTree/WinBinaryTree/BinaryTree.h
@@ -0,0 +1,43 @@
+#pragma once
+#include <wtypes.h>
+
+class BinaryTree
+{
+private:
+	struct Node
+	{
+		int data;
+
+		Node* parent;
+		Node* left;
+		Node* right;
+
+		Node(int data)
+			: data(data)
+			, parent(nullptr)
+			, left(nullptr)
+			, right(nullptr)
+		{
+		}
+	};
+
+public:
+	BinaryTree();
+	~BinaryTree();
+	void InsertNode(int data);
+	void printTree();
+	void printTreeWin(HWND hWnd);
+	bool DeleteNode(int data);
+	
+
+private:
+	Node* SearchHelper(int data);
+	Node* SearchMax(Node* root);
+	Node* SearchMin(Node* root);
+	void printHelper(Node* root, int depth);
+	void printWinHelper(HDC hdc, Node* root, int depth);
+	void deleteHelper(Node* root);
+
+private:
+	BinaryTree::Node* mRoot;
+};
