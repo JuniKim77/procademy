acc55c927720f8fb38ca9b5125adfec7f79e6ca1
hojun3775 <hojun3775@gmail.com>
2021-06-04 11:55:53 +0900

    # 레드 블랙 원래 라이브러리 업데이트

--- redBlackTree/redBlackTree/RedBlackTree.cpp
+++ redBlackTree/redBlackTree/RedBlackTree.cpp
@@ -6,6 +6,8 @@ RedBlackTree::RedBlackTree()
 	Nil = new Node(-1);
 	Nil->color = NODE_COLOR::BLACK;
 	mRoot = Nil;
+	mRedBrush = CreateSolidBrush(RGB(150, 0, 0));
+	mBlackBrush = CreateSolidBrush(RGB(0, 0, 0));
 }
 
 RedBlackTree::~RedBlackTree()
@@ -43,6 +45,7 @@ void RedBlackTree::InsertNode(int data)
 				node->left = newNode;
 
 				// Balancing
+				InsertRebalance(newNode);
 
 				return;
 			}
@@ -60,6 +63,7 @@ void RedBlackTree::InsertNode(int data)
 				node->right = newNode;
 
 				// Balancing
+				InsertRebalance(newNode);
 
 				return;
 			}
@@ -73,12 +77,12 @@ bool RedBlackTree::DeleteNode(int data)
 {
 	Node* pNode = SearchHelper(data);
 
-	if (pNode == nullptr)
+	if (pNode == Nil)
 		return false;
 
 	// 데이터가 발견된 경우
 	// 양 노드를 다 갖고 있는 경우
-	if (pNode->left != nullptr && pNode->right != nullptr)
+	if (pNode->left != Nil && pNode->right != Nil)
 	{
 		Node* rightMin = SearchMin(pNode->right);
 
@@ -95,19 +99,22 @@ bool RedBlackTree::DeleteNode(int data)
 			parent->right = rightMin->right;
 		}
 
-		if (rightMin->right != nullptr)
+		rightMin->right->parent = parent;
+
+		if (rightMin->color == NODE_COLOR::BLACK)
 		{
-			rightMin->right->parent = parent;
+			DeleteRebalance(rightMin->right);
 		}
 
 		delete rightMin;
 	}
 	// 하나의 자식만 갖은 경우
-	else if (pNode->left != nullptr)
+	else if (pNode->left != Nil)
 	{
 		if (pNode == mRoot)
 		{
 			mRoot = pNode->left;
+			mRoot->color = NODE_COLOR::BLACK;
 			delete pNode;
 
 			return true;
@@ -126,9 +133,14 @@ bool RedBlackTree::DeleteNode(int data)
 
 		pNode->left->parent = parent;
 
+		if (pNode->color == NODE_COLOR::BLACK)
+		{
+			DeleteRebalance(pNode->left);
+		}
+
 		delete pNode;
 	}
-	else if (pNode->right != nullptr)
+	else if (pNode->right != Nil)
 	{
 		if (pNode == mRoot)
 		{
@@ -151,6 +163,11 @@ bool RedBlackTree::DeleteNode(int data)
 
 		pNode->right->parent = parent;
 
+		if (pNode->color == NODE_COLOR::BLACK)
+		{
+			DeleteRebalance(pNode->right);
+		}
+
 		delete pNode;
 	}
 	// 아무 자식 없는 경우
@@ -158,25 +175,93 @@ bool RedBlackTree::DeleteNode(int data)
 	{
 		if (pNode == mRoot)
 		{
-			mRoot = nullptr;
+			mRoot = Nil;
 			delete pNode;
 
 			return true;
 		}
 
-		if (pNode->parent->right == pNode)
+		Node* parent = pNode->parent;
+
+		if (parent->right == pNode)
 		{
-			pNode->parent->right = nullptr;
+			parent->right = Nil;
 		}
 		else
 		{
-			pNode->parent->left = nullptr;
+			parent->left = Nil;
+		}
+
+		Nil->parent = parent;
+
+		if (pNode->color == NODE_COLOR::BLACK)
+		{
+			DeleteRebalance(Nil);
 		}
 
 		delete pNode;
 	}
 
 	return true;
+}
+
+void RedBlackTree::printTreeWin(HWND hWnd)
+{
+	HDC hdc = GetDC(hWnd);
+	SetBkMode(hdc, TRANSPARENT);
+	SetTextAlign(hdc, TA_CENTER);
+	SetTextColor(hdc, RGB(200, 200, 200));
+
+	int x = GetSystemMetrics(SM_CXSCREEN);
+
+	printWinHelper(hdc, mRoot, 0, x, 0);
+
+	ReleaseDC(hWnd, hdc);
+}
+
+void RedBlackTree::printWinHelper(HDC hdc, Node* root, int beginX, int endX, int depth)
+{
+	if (root == Nil)
+		return;
+
+	int x = (beginX + endX) / 2;
+	int y = 100 * depth;
+
+	drawNode(hdc, root, x, y, (beginX + x) / 2, (x + endX) / 2);
+
+	printWinHelper(hdc, root->left, beginX, x, depth + 1);
+	printWinHelper(hdc, root->right, x, endX, depth + 1);
+}
+
+void RedBlackTree::drawNode(HDC hdc, Node* node, int x, int y, int leftX, int rightX)
+{
+	// 선 그리기
+	if (node->left != Nil)
+	{
+		MoveToEx(hdc, x + 25, y + 17, NULL);
+		LineTo(hdc, leftX + 25, y + 117);
+	}
+
+	if (node->right != Nil)
+	{
+		MoveToEx(hdc, x + 25, y + 17, NULL);
+		LineTo(hdc, rightX + 25, y + 117);
+	}
+
+	if (node->color == NODE_COLOR::BLACK)
+	{
+		SelectObject(hdc, mBlackBrush);
+	}
+	else
+	{
+		SelectObject(hdc, mRedBrush);
+	}
+
+	Ellipse(hdc, x, y, x + 50, y + 50);
+	WCHAR text[20];
+	wsprintf(text, L"%d", node->data);
+
+	TextOut(hdc, x + 25, y + 17, text, wcslen(text));
 }
 
 RedBlackTree::Node* RedBlackTree::SearchHelper(int data)
@@ -319,4 +404,277 @@ void RedBlackTree::RotateLeft(Node* root)
 	// 3. Right를 Parent로 승격
 	right->left = root;
 	root->parent = right;
+}
+
+void RedBlackTree::InsertRebalance(Node* originRoot)
+{
+	Node* root = originRoot;
+
+	while (1)
+	{
+		if (root == mRoot)
+		{
+			root->color = NODE_COLOR::BLACK;
+			return;
+		}
+
+		if (root->parent->color == NODE_COLOR::BLACK)
+			return;
+
+		Node* parent = root->parent;
+		Node* grand = parent->parent;
+
+		if (grand->left == parent)
+		{
+			if (grand->right->color == NODE_COLOR::BLACK)
+			{
+				Left_InsertRebalanceUncleBlack(root);
+
+				return;
+			}
+			else
+			{
+				root = InsertRebalanceUncleRed(root);
+			}
+		}
+		else
+		{
+			if (grand->left->color == NODE_COLOR::BLACK)
+			{
+				Right_InsertRebalanceUncleBlack(root);
+
+				return;
+			}
+			else
+			{
+				root = InsertRebalanceUncleRed(root);
+			}
+		}
+	}
+}
+
+void RedBlackTree::Left_InsertRebalanceUncleBlack(Node* root)
+{
+	Node* parent = root->parent;
+	Node* grand = parent->parent;
+
+	if (parent->right == root)
+	{
+		RotateLeft(parent);
+		root = parent;
+		parent = root->parent;
+	}
+
+	grand->color = NODE_COLOR::RED;
+	parent->color = NODE_COLOR::BLACK;
+	RotateRight(grand);
+
+	if (grand == mRoot)
+	{
+		mRoot = parent;
+	}
+}
+
+void RedBlackTree::Right_InsertRebalanceUncleBlack(Node* root)
+{
+	Node* parent = root->parent;
+	Node* grand = parent->parent;
+
+	if (parent->left == root)
+	{
+		RotateRight(parent);
+		root = parent;
+		parent = root->parent;
+	}
+
+	grand->color = NODE_COLOR::RED;
+	parent->color = NODE_COLOR::BLACK;
+	RotateLeft(grand);
+
+	if (grand == mRoot)
+	{
+		mRoot = parent;
+	}
+}
+
+RedBlackTree::Node* RedBlackTree::InsertRebalanceUncleRed(Node* root)
+{
+	Node* parent = root->parent;
+	Node* grand = parent->parent;
+	Node* uncle = grand->left == parent ? grand->right : grand->left;
+
+	grand->color = NODE_COLOR::RED;
+	parent->color = NODE_COLOR::BLACK;
+	uncle->color = NODE_COLOR::BLACK;
+
+	return grand;
+}
+
+void RedBlackTree::DeleteRebalance(Node* originRoot)
+{
+	Node* root = originRoot;
+
+	while (1)
+	{
+		if (root->color == NODE_COLOR::RED)
+		{
+			root->color = NODE_COLOR::BLACK;
+			return;
+		}
+
+		Node* parent = root->parent;
+
+		// 왼쪽편인 경우
+		if (parent->left == root)
+		{
+			Node* sibling = parent->right;
+
+			if (sibling->color == NODE_COLOR::RED)
+			{
+				Left_DeleteRebalanceSiblingRed(sibling);
+			}
+			else
+			{
+				if (sibling->left->color == NODE_COLOR::BLACK &&
+					sibling->right->color == NODE_COLOR::BLACK)
+				{
+					root = Both_DeleteRebalanceSiblingBlackBothBlack(sibling);
+				}
+				else
+				{
+					if (sibling->left->color == NODE_COLOR::RED)
+					{
+						Left_DeleteRebalanceSiblingBlackInsideRed(sibling);
+					}
+
+					sibling = parent->right;
+
+					Left_DeleteRebalanceSiblingBlackOutsideRed(sibling);
+
+					return;
+				}
+			}
+		}
+		// 오른쪽편인 경우
+		else
+		{
+			Node* sibling = parent->left;
+
+			if (sibling->color == NODE_COLOR::RED)
+			{
+				Right_DeleteRebalanceSiblingRed(sibling);
+			}
+			else
+			{
+				if (sibling->left->color == NODE_COLOR::BLACK &&
+					sibling->right->color == NODE_COLOR::BLACK)
+				{
+					root = Both_DeleteRebalanceSiblingBlackBothBlack(sibling);
+				}
+				else
+				{
+					if (sibling->right->color == NODE_COLOR::RED)
+					{
+						Right_DeleteRebalanceSiblingBlackInsideRed(sibling);
+					}
+
+					sibling = parent->left;
+
+					Right_DeleteRebalanceSiblingBlackOutsideRed(sibling);
+
+					return;
+				}
+			}
+		}
+	}
+}
+
+void RedBlackTree::Left_DeleteRebalanceSiblingRed(Node* root)
+{
+	Node* parent = root->parent;
+
+	parent->color = NODE_COLOR::RED;
+	root->color = NODE_COLOR::BLACK;
+
+	RotateLeft(parent);
+
+	if (mRoot == parent)
+	{
+		mRoot = root;
+		mRoot->color = NODE_COLOR::BLACK;
+	}
+}
+
+void RedBlackTree::Right_DeleteRebalanceSiblingRed(Node* root)
+{
+	Node* parent = root->parent;
+
+	parent->color = NODE_COLOR::RED;
+	root->color = NODE_COLOR::BLACK;
+
+	RotateRight(parent);
+
+	if (mRoot == parent)
+	{
+		mRoot = root;
+		mRoot->color = NODE_COLOR::BLACK;
+	}
+}
+
+RedBlackTree::Node* RedBlackTree::Both_DeleteRebalanceSiblingBlackBothBlack(Node* root)
+{
+	Node* parent = root->parent;
+	root->color = NODE_COLOR::RED;
+
+	return parent;
+}
+
+void RedBlackTree::Left_DeleteRebalanceSiblingBlackInsideRed(Node* root)
+{
+	root->color = NODE_COLOR::RED;
+	root->left->color = NODE_COLOR::BLACK;
+
+	RotateRight(root);
+}
+
+void RedBlackTree::Right_DeleteRebalanceSiblingBlackInsideRed(Node* root)
+{
+	root->color = NODE_COLOR::RED;
+	root->right->color = NODE_COLOR::BLACK;
+
+	RotateLeft(root);
+}
+
+void RedBlackTree::Left_DeleteRebalanceSiblingBlackOutsideRed(Node* root)
+{
+	Node* parent = root->parent;
+
+	root->color = parent->color;
+	parent->color = NODE_COLOR::BLACK;
+	root->right->color = NODE_COLOR::BLACK;
+
+	RotateLeft(parent);
+
+	if (mRoot == parent)
+	{
+		mRoot = root;
+		mRoot->color = NODE_COLOR::BLACK;
+	}
+}
+
+void RedBlackTree::Right_DeleteRebalanceSiblingBlackOutsideRed(Node* root)
+{
+	Node* parent = root->parent;
+
+	root->color = parent->color;
+	parent->color = NODE_COLOR::BLACK;
+	root->left->color = NODE_COLOR::BLACK;
+
+	RotateRight(parent);
+
+	if (mRoot == parent)
+	{
+		mRoot = root;
+		mRoot->color = NODE_COLOR::BLACK;
+	}
 }
---
+++ redBlackTree/redBlackTree/main.cpp
@@ -0,0 +1,8 @@
+#include "RedBlackTree.h"
+
+int main()
+{
+
+
+	return 0;
+}
