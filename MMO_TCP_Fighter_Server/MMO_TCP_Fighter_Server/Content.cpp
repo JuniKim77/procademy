125645e444fb7dcabbda248ade50aeb98f469a16
hojun3775 <hojun3775@gmail.com>
2021-07-02 15:59:00 +0900

    # 컨텐츠 파트 틀 구축 완료

--- MMO_TCP_Fighter_Server/MMO_TCP_Fighter_Server/Content.cpp
+++ MMO_TCP_Fighter_Server/MMO_TCP_Fighter_Server/Content.cpp
@@ -1,6 +1,57 @@
 #include "Content.h"
+#include <stdio.h>
+#include "PacketDefine.h"
 
 bool PacketProc(DWORD from, WORD msgType, CPacket* packet)
+{
+	wprintf_s(L"패킷 수신 [UserNo: %d][msgType: %d]\n", from, msgType);
+
+	switch (msgType)
+	{
+	case dfPACKET_CS_MOVE_START:
+		return CS_MoveStart(from, packet);
+		break;
+	case dfPACKET_CS_MOVE_STOP:
+		return CS_MoveStop(from, packet);
+		break;
+	case dfPACKET_CS_ATTACK1:
+		return CS_Attack1(from, packet);
+		break;
+	case dfPACKET_CS_ATTACK2:
+		return CS_Attack2(from, packet);
+		break;
+	case dfPACKET_CS_ATTACK3:
+		return CS_Attack3(from, packet);
+		break;
+	default:
+		// Log...
+		break;
+	}
+
+	return true;
+}
+
+bool CS_MoveStart(DWORD from, CPacket* packet)
+{
+	return false;
+}
+
+bool CS_MoveStop(DWORD from, CPacket* packet)
+{
+	return false;
+}
+
+bool CS_Attack1(DWORD from, CPacket* packet)
+{
+	return false;
+}
+
+bool CS_Attack2(DWORD from, CPacket* packet)
+{
+	return false;
+}
+
+bool CS_Attack3(DWORD from, CPacket* packet)
 {
 	return false;
 }
--- MMO_TCP_Fighter_Server/MMO_TCP_Fighter_Server/Content.h
+++ MMO_TCP_Fighter_Server/MMO_TCP_Fighter_Server/Content.h
@@ -6,3 +6,8 @@ class CPacket;
 struct stHeader;
 
 bool PacketProc(DWORD from, WORD msgType, CPacket* packet);
+bool CS_MoveStart(DWORD from, CPacket* packet);
+bool CS_MoveStop(DWORD from, CPacket* packet);
+bool CS_Attack1(DWORD from, CPacket* packet);
+bool CS_Attack2(DWORD from, CPacket* packet);
+bool CS_Attack3(DWORD from, CPacket* packet);
