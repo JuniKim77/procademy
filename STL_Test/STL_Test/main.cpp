c67b21aad426d0be70f7d8c9a936e72806ffc180
hojun3775 <hojun3775@gmail.com>
2021-07-06 16:32:40 +0900

    # 테스트 케이스 추가 및 호출 보완

--- STL_Test/STL_Test/main.cpp
+++ STL_Test/STL_Test/main.cpp
@@ -15,9 +15,16 @@ public:
 		cout << "Test Constructor" << endl;
 	}
 
+	TEST(int num)
+	{
+		cout << "Test Constructor with Num" << endl;
+		mNum = num;
+	}
+
 	TEST(const TEST& other)
 	{
-		cout << "Test Copy Constructor" << endl;
+		mNum = other.mNum;
+		cout << "Test Copy Constructor [Num: " << mNum << "]" << endl;
 	}
 
 	void operator=(const TEST& other)
@@ -27,7 +34,7 @@ public:
 
 	~TEST()
 	{
-		cout << "Test Destructor" << endl;
+		cout << "Test Destructor [Num: " << mNum << "]" << endl;
 	}
 
 	int mNum;
@@ -39,27 +46,59 @@ int main()
 		vector<TEST> test;
 		//test.reserve(60);
 
-		TEST t1;
+		TEST t1(1);
 
 		test.push_back(t1);
 
-		TEST t2;
+		TEST t2(2);
 
 		test.push_back(t2);
 
-		TEST t3;
+		TEST t3(3);
 
 		test.push_back(t3);
 
-		TEST t4;
+		TEST t4(4);
 
 		test.push_back(t4);
 
-		TEST t5;
+		TEST t5(5);
 
 		test.push_back(t5);
 
-		for (int i = 0; i < 5; ++i)
+		TEST t6(6);
+
+		test.push_back(t6);
+
+		TEST t7(7);
+
+		test.push_back(t7);
+
+		TEST t8(8);
+
+		test.push_back(t8);
+
+		TEST t9(9);
+
+		test.push_back(t9);
+
+		TEST t10(10);
+
+		test.push_back(t10);
+
+		TEST t11(11);
+
+		test.push_back(t11);
+
+		TEST t12(12);
+
+		test.push_back(t12);
+
+		TEST t13(13);
+
+		test.push_back(t13);
+
+		for (int i = 0; i < 13; ++i)
 		{
 			cout << test[i].mNum << " ";
 		}
