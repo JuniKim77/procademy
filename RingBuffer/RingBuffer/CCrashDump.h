f84a3edd6d36de35ba40a6fdd4507a9dfa702ed8
김호준 <hojun3775@gmail.com>
2021-09-18 20:26:50 +0900

    # 디버깅 도구 추가

---
+++ RingBuffer/RingBuffer/CCrashDump.h
@@ -0,0 +1,44 @@
+/*****************************************************************************
+Module   : CrashDump
+Notices   : 테스트 미실행
+Todo   :
+*****************************************************************************/
+#pragma once
+#pragma comment (lib, "Dbghelp.lib")
+#include <direct.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <windows.h>
+#include <Dbghelp.h>
+#include <psapi.h>
+#include <crtdbg.h>
+
+#define CRASH() procademy::CCrashDump::Crash()
+
+namespace procademy
+{
+    class CCrashDump
+    {
+    public:
+        CCrashDump();
+
+        ~CCrashDump()
+        {
+
+        }
+
+        static void Crash(void);
+
+        static LONG WINAPI MyExceptionFilter(__in PEXCEPTION_POINTERS pExceptionPointer);
+
+        static void SetHandlerDump(void);
+
+        static void myInvalidParameterHandler(const wchar_t* expression, const wchar_t* function, const wchar_t* file, unsigned int   line, uintptr_t   pReserved);
+
+        static int _custom_Report_hook(int ireposttype, char* message, int* returnvalue);
+
+        static void myPurecallHandler(void);
+
+        static long _DumpCount;
+    };
+}
