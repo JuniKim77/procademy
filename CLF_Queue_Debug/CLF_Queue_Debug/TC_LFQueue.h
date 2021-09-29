3ec2d8a7b45187a0defcdb2633856a5dac396da6
hojun3775 <hojun3775@gmail.com>
2021-09-29 15:49:06 +0900

    # MoveTail 리팩토링 추가

--- CLF_Queue_Debug/CLF_Queue_Debug/TC_LFQueue.h
+++ CLF_Queue_Debug/CLF_Queue_Debug/TC_LFQueue.h
@@ -102,6 +102,9 @@ public:
 	DWORD GetPoolSize() { return mMemoryPool.GetSize(); }
 	void linkCheck(int size);
 	void Log(int logicId, t_Top snap_top, Node* next, bool isHead = false);
+
+private:
+	void MoveTail(t_Top* snap, Node** next);
 };
 
 template<typename DATA>
@@ -150,23 +153,23 @@ inline void TC_LFQueue<DATA>::Enqueue(DATA data)
 	{
 		do
 		{
-			top.counter = mTail.counter;
-			top.ptr_node = mTail.ptr_node;
-			next = top.ptr_node->next;
+			//top.counter = mTail.counter;
+			//top.ptr_node = mTail.ptr_node;
+			//next = top.ptr_node->next;
 
-			if (next != nullptr)
-			{
-				//Log(LOGIC_ENQUEUE + 5, top, next);
-				if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
-				{
-					//Log(LOGIC_ENQUEUE + 10, top, next);
-				}
-				else
-				{
-					Log(LOGIC_ENQUEUE + 20, top, next);
-				}
-				
-			}
+			//if (next != nullptr)
+			//{
+			//	//Log(LOGIC_ENQUEUE + 5, top, next);
+			//	if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
+			//	{
+			//		//Log(LOGIC_ENQUEUE + 10, top, next);
+			//	}
+			//	else
+			//	{
+			//		Log(LOGIC_ENQUEUE + 20, top, next);
+			//	}
+			//}
+			MoveTail(&top, &next);
 		} while (next != nullptr);
 
 		//Log(LOGIC_ENQUEUE + 30, top, next);
@@ -175,22 +178,24 @@ inline void TC_LFQueue<DATA>::Enqueue(DATA data)
 			InterlockedIncrement((DWORD*)&mSize);
 			Log(LOGIC_ENQUEUE + 40, top, next);
 
-			top.counter = mTail.counter;
-			top.ptr_node = mTail.ptr_node;
-			next = top.ptr_node->next;
+			MoveTail(&top, &next);
 
-			if (next != nullptr)
-			{
-				//Log(LOGIC_ENQUEUE + 50, top, next, true);
-				if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
-				{
-					//Log(LOGIC_ENQUEUE + 60, top, next, true);
-				}
-				else
-				{
-					Log(LOGIC_ENQUEUE + 70, top, next);
-				}
-			}
+			//top.counter = mTail.counter;
+			//top.ptr_node = mTail.ptr_node;
+			//next = top.ptr_node->next;
+
+			//if (next != nullptr)
+			//{
+			//	//Log(LOGIC_ENQUEUE + 50, top, next, true);
+			//	if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
+			//	{
+			//		//Log(LOGIC_ENQUEUE + 60, top, next, true);
+			//	}
+			//	else
+			//	{
+			//		Log(LOGIC_ENQUEUE + 70, top, next);
+			//	}
+			//}
 
 			break;
 		}
@@ -219,22 +224,23 @@ inline bool TC_LFQueue<DATA>::Dequeue(DATA* data)
 	{
 		if (mHead.ptr_node == mTail.ptr_node)
 		{
-			top.counter = mTail.counter;
-			top.ptr_node = mTail.ptr_node;			
-			next = top.ptr_node->next;
+			//top.counter = mTail.counter;
+			//top.ptr_node = mTail.ptr_node;			
+			//next = top.ptr_node->next;
 
-			if (next != nullptr)
-			{
-				//Log(LOGIC_DEQUEUE + 10, top, nullptr);
-				if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
-				{
-					//Log(LOGIC_DEQUEUE + 20, top, nullptr);
-				}
-				else
-				{
-					Log(LOGIC_DEQUEUE + 30, top, nullptr);
-				}
-			}
+			//if (next != nullptr)
+			//{
+			//	//Log(LOGIC_DEQUEUE + 10, top, nullptr);
+			//	if (InterlockedCompareExchange128((LONG64*)&mTail, top.counter + 1, (LONG64)next, (LONG64*)&top) == 0)
+			//	{
+			//		//Log(LOGIC_DEQUEUE + 20, top, nullptr);
+			//	}
+			//	else
+			//	{
+			//		Log(LOGIC_DEQUEUE + 30, top, nullptr);
+			//	}
+			//}
+			MoveTail(&top, &next);
 		}
 		
 		do
@@ -292,4 +298,25 @@ inline void TC_LFQueue<DATA>::Log(int logicId, t_Top snap_top, Node* next, bool 
 		_Log(logicId, GetCurrentThreadId(), mSize, mTail.counter, snap_top.counter, mHead.ptr_node, mHead.ptr_node->next, mTail.ptr_node, mTail.ptr_node->next, snap_top.ptr_node, next);
 	}
 	
+}
+
+template<typename DATA>
+inline void TC_LFQueue<DATA>::MoveTail(t_Top* snap, Node** next)
+{
+	snap->counter = mTail.counter;
+	snap->ptr_node = mTail.ptr_node;
+	*next = snap->ptr_node->next;
+
+	if (*next != nullptr)
+	{
+		//Log(LOGIC_DEQUEUE + 10, top, nullptr);
+		if (InterlockedCompareExchange128((LONG64*)&mTail, snap->counter + 1, (LONG64)*next, (LONG64*)snap) == 0)
+		{
+			//Log(LOGIC_DEQUEUE + 20, top, nullptr);
+		}
+		else
+		{
+			//Log(LOGIC_DEQUEUE + 30, top, nullptr);
+		}
+	}
 }
