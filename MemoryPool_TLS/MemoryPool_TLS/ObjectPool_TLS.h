6f74713d870cf2f03e928749bf5b1bc72cf0c33c
김호준 <hojun3775@gmail.com>
2021-10-20 18:18:21 +0900

    # 메모리풀 TLS 완성

--- MemoryPool_TLS/MemoryPool_TLS/ObjectPool_TLS.h
+++ MemoryPool_TLS/MemoryPool_TLS/ObjectPool_TLS.h
@@ -1,5 +1,19 @@
+#pragma once
 #include "TC_LFObjectPool.h"
 #include <wtypes.h>
+
+struct packetDebug;
+
+extern USHORT g_debugIdx;
+extern packetDebug g_packetDebugs[USHRT_MAX + 1];
+extern void packetLog(
+	int			logicId,
+	DWORD		threadId,
+	void* pChunk,
+	void* pPacket,
+	int			allocCount,
+	LONG		freeCount
+);
 
 #define CHUNK_CHECKSUM (0xBBBBBBBB)
 
@@ -15,7 +29,7 @@ namespace procademy
 		class CChunk
 		{
 		public:
-			DATA*	Alloc(void);
+			DATA* Alloc(void);
 			bool	Free(DATA* pData);
 
 			enum {
@@ -23,9 +37,10 @@ namespace procademy
 			};
 
 			st_Chunk_Block				mArray[MAX_SIZE];
-			ObjectPool_TLS*				pObjPool = nullptr;
+			ObjectPool_TLS* pObjPool = nullptr;
 			int							mAllocCount = 0;
 			alignas(64) st_FreeCount	mFreeCount;
+			DWORD						threadID;
 		};
 
 		struct st_FreeCount
@@ -37,22 +52,22 @@ namespace procademy
 				{
 					SHORT freeCount;
 					SHORT isFreed;
-				} freeStaus;
+				} freeStatus;
 			};
 		};
 
 		struct st_Chunk_Block
 		{
-			DATA	data;
-			void*	code;
+			DATA			data;
+			void* code;
 			CChunk* pOrigin;
-			unsigned int checkSum_over = CHUNK_CHECKSUM;
+			unsigned int	checkSum_over = CHUNK_CHECKSUM;
 		};
 
 	public:
 		ObjectPool_TLS(bool bPlacementNew = false, bool sizeCheck = false);
 		virtual	~ObjectPool_TLS();
-		DATA*	Alloc(void);
+		DATA* Alloc(void);
 		bool	Free(DATA* pData);
 		int		GetCapacity(void);
 		DWORD	GetSize(void) { return mSize; }
@@ -88,15 +103,15 @@ namespace procademy
 	{
 		CChunk* chunk = (CChunk*)TlsGetValue(mIndex);
 
-		if (chunk == nullptr || chunk->mAllocCount == CChunk::MAX_SIZE)
+		if (chunk == nullptr || chunk->threadID != GetCurrentThreadId() || chunk->mAllocCount == CChunk::MAX_SIZE)
 		{
 			chunk = mMemoryPool->Alloc();
-
+			//packetLog(30040, GetCurrentThreadId(), chunk, nullptr, chunk->mAllocCount, chunk->mFreeCount.freeStatus.freeCount);
+			chunk->threadID = GetCurrentThreadId();
 			chunk->pObjPool = this;
 			chunk->mAllocCount = 0;
-			chunk->mFreeCount.freeStaus.freeCount = 0;
-			chunk->mFreeCount.freeStaus.isFreed = 0;
-
+			chunk->mFreeCount.freeStatus.freeCount = 0;
+			chunk->mFreeCount.freeStatus.isFreed = 0;
 			TlsSetValue(mIndex, chunk);
 		}
 
@@ -123,6 +138,8 @@ namespace procademy
 			return nullptr;
 
 		st_Chunk_Block* pBlock = (st_Chunk_Block*)&mArray[mAllocCount++];
+
+		//packetLog(20000, GetCurrentThreadId(), this, pBlock, mAllocCount, mFreeCount.freeStatus.freeCount);
 		pBlock->pOrigin = this;
 		pBlock->code = this;
 		pBlock->checkSum_over = CHUNK_CHECKSUM;
@@ -142,14 +159,17 @@ namespace procademy
 			return false;
 		}
 
-		InterlockedIncrement((LONG*)&mFreeCount.counter);
+		InterlockedIncrement((LONG*)&mFreeCount);
 
 		freeCounter.counter = CChunk::MAX_SIZE;
-		freeCounter.freeStaus.isFreed = 1;
-
+		freeCounter.freeStatus.isFreed = 1;
+		//packetLog(10000, GetCurrentThreadId(), this, pData, mAllocCount, mFreeCount.freeStatus.freeCount);
 		if (InterlockedCompareExchange(&mFreeCount.counter, freeCounter.counter, CChunk::MAX_SIZE) == CChunk::MAX_SIZE)
 		{
+			//packetLog(10020, GetCurrentThreadId(), this, pData, mAllocCount, mFreeCount.freeStatus.freeCount);
+			block->pOrigin->threadID = 0;
 			pObjPool->mMemoryPool->Free(block->pOrigin);
+			//packetLog(10030, GetCurrentThreadId(), this, pData, mAllocCount, mFreeCount.freeStatus.freeCount);
 		}
 
 		return true;
--- MemoryPool_TLS/MemoryPool_TLS/main.cpp
+++ MemoryPool_TLS/MemoryPool_TLS/main.cpp
@@ -19,6 +19,38 @@ public:
 	int mNum = 0;
 };
 
+struct packetDebug
+{
+	int			logicId;
+	DWORD		threadId;
+	void* pChunk;
+	void* pPacket;
+	int			allocCount;
+	LONG		freeCount;
+};
+
+USHORT g_debugIdx = 0;
+packetDebug g_packetDebugs[USHRT_MAX + 1] = { 0, };
+
+void packetLog(
+	int			logicId = -9999,
+	DWORD		threadId = 0,
+	void* pChunk = nullptr,
+	void* pPacket = nullptr,
+	int			allocCount = -9999,
+	LONG		freeCount = 9999
+)
+{
+	USHORT index = (USHORT)InterlockedIncrement16((short*)&g_debugIdx);
+
+	g_packetDebugs[index].logicId = logicId;
+	g_packetDebugs[index].threadId = threadId;
+	g_packetDebugs[index].pChunk = pChunk;
+	g_packetDebugs[index].pPacket = pPacket;
+	g_packetDebugs[index].allocCount = allocCount;
+	g_packetDebugs[index].freeCount = freeCount;
+}
+
 int main()
 {
 	procademy::ObjectPool_TLS<TEST> tt;
