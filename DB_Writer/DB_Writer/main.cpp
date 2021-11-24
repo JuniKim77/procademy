3e6f23496c345ec678e479f3fb65160359e9f16f
김호준 <hojun3775@gmail.com>
2021-11-24 11:31:10 +0900

    # account db 조회 구문 테스트

--- DB_Writer/DB_Writer/main.cpp
+++ DB_Writer/DB_Writer/main.cpp
@@ -4,27 +4,27 @@
 
 int main()
 {
-	procademy::CDBConnector db(L"127.0.0.1", L"root", L"123456789", L"test", 3306);
+	procademy::CDBConnector db(L"127.0.0.1", L"root", L"123456789", L"accountdb", 3306);
 	WCHAR temp[10];
 	MYSQL_ROW sql_row;
 	bool ret;
 
-	for (int i = 0; i < 100; ++i)
-	{
-		swprintf_s(temp, _countof(temp), L"test_%d", i);
-		ret = db.Query_Save(L"INSERT INTO test.item (item_id, name, value) VALUES (%d, '%s', %d)", i, temp, i * 10);
+	//for (int i = 0; i < 100; ++i)
+	//{
+	//	swprintf_s(temp, _countof(temp), L"test_%d", i);
+	//	ret = db.Query_Save(L"INSERT INTO test.item (item_id, name, value) VALUES (%d, '%s', %d)", i, temp, i * 10);
 
-		if (!ret)
-		{
-			CLogger::_Log(dfLOG_LEVEL_ERROR, L"Query Error\n");
-		}
-	}
+	//	if (!ret)
+	//	{
+	//		CLogger::_Log(dfLOG_LEVEL_ERROR, L"Query Error\n");
+	//	}
+	//}
 
-	db.Query(L"SELECT * FROM test.item");
+	db.Query(L"SELECT `accountno`, `userid`, `usernick` FROM `accountdb`.`account` LIMIT 10;");
 
 	while ((sql_row = db.FetchRow()) != NULL)
 	{
-		printf("%3s %10s %s\n", sql_row[0], sql_row[1], sql_row[2]);
+		printf("%3s %10s %10s\n", sql_row[0], sql_row[1], sql_row[2]);
 	}
 
 	db.Disconnect();
